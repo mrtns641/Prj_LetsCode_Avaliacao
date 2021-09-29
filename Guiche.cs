f6c4b9504db9e38ff9f6e8da5f9e6dac2863da9c
Rodrigo-Paim <rodrigo_vasconceloss@hotmail.com>
2021-09-28 21:11:39 -0300

    relalizado alterações na classe passagem e criado a classe guiche

---
+++ Guiche.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace Prj_LetsCode_Avaliacao
+{
+  public class Guiche : ILoja
+  {
+    private Passagem passagem;
+    private Passageiros passageiro;
+    private Aeronave aeronave;
+    private string destino;
+    private int assento;
+
+    private double valor;
+
+    public int Assento
+    {
+      get { return assento; }
+      set { assento = value; }
+    }
+    private Bagagem bagagem;
+    private string nome;
+    public string Nome
+    {
+      get { return nome; }
+      set { nome = value; }
+    }
+
+    public virtual void Vender()
+    {
+      passagem.Assento = assento;
+      passagem.Passageiro = passageiro;
+      passagem.Aeronave = aeronave;
+      passagem.Destino = destino;
+      passagem.Valor = valor;
+
+      Console.WriteLine($"{passageiro.Nome}, seu voo com destino a {passagem.Destino}");
+      Console.WriteLine($"será na aeronave {aeronave.Modelo} no valor de R$: {passagem.Valor}");
+      Console.WriteLine($"Venda Finalizada, obrigado por voar {Nome} !");
+
+
+    }
+
+    public Guiche(string nome, Passageiros passageiro, Aeronave aeronave, Bagagem bagagem, int assento, double Valor)
+    {
+      this.passageiro = passageiro;
+      this.nome = nome;
+      this.aeronave = aeronave;
+      this.bagagem = bagagem;
+      this.assento = assento;
+      this.valor = Valor;
+    }
+  }
+}
--- Passagem.cs
+++ Passagem.cs
@@ -2,31 +2,55 @@ using System;
 
 namespace Prj_LetsCode_Avaliacao
 {
-    public class Passagem
+  public class Passagem
+  {
+
+    private int assento;
+    private double valor;
+    public double Valor
     {
-        
-        private string nomePassageiro;
-        private int assento;
-
-        public string NomePassageiro
-        {
-            get { return nomePassageiro;}
-            set { nomePassageiro = value;}
-        }
-        
-        public int Assento
-        {
-            get { return assento;}
-            set { assento = value;}
-        }
-
-        // Construtor da Classe Passagem
-        public Passagem(string nomePassageiro, int assento){
-            this.NomePassageiro = nomePassageiro;
-            this.Assento = assento;
-
-        }
-
+      get { return valor; }
+      set { valor = value; }
     }
+
+    private string destino;
+    public string Destino
+    {
+      get { return destino; }
+      set { destino = value; }
+    }
+
+    private Passageiros passageiro;
+    public Passageiros Passageiro
+    {
+      get { return passageiro; }
+      set { passageiro = value; }
+    }
+
+    private Aeronave aeronave;
+    public Aeronave Aeronave
+    {
+      get { return aeronave; }
+      set { aeronave = value; }
+    }
+
+
+    public int Assento
+    {
+      get { return assento; }
+      set { assento = value; }
+    }
+
+    // Construtor da Classe Passagem
+    public Passagem(Passageiros Passageiro, Aeronave Aeronave, int assento, string destino, double valor)
+    {
+      this.aeronave = Aeronave;
+      this.passageiro = Passageiro;
+      this.Assento = assento;
+      this.Destino = destino;
+      this.Valor = valor;
+    }
+
+  }
 
 }
